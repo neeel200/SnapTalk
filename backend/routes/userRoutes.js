23f7bf2c8ffbdf941104262cccd85fa88b0f23c4
neelesh <neeleshvinchoorkar@gmail.com>
2024-07-21 23:53:35 +0530

    s3 implemented in frontend and backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,12 +7,14 @@ const {
 } = require("../controllers/userControllers.js");
 
 const { protect } = require("../middleware/authMiddleware.js");
-
+const { getS3SignedURL,
+  getProfileImageFromS3 } = require("../utils/s3Service.js")
 const router = express.Router();
 
 router.route("/").get(protect, allUsers);
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-
+router.get("/presignedurl", getS3SignedURL);
+router.get("/profile/image", getProfileImageFromS3);
 
 module.exports = router;
