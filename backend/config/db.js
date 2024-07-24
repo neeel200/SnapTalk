5ae17f21bc132c517d9f58dc2d7e678245223aeb
neelesh <neeleshvinchoorkar@gmail.com>
2024-07-24 08:05:57 +0530

    db config file updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,8 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       dbName: "chatApplication",
+      useUnifiedTopology: true,
+      useNewUrlParser: true
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
